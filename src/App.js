a6ba59a6263a03a065354a8c43b66738a24cb4d3
Benjamin <benjaminalexandremail@gmail.com>
2025-10-01 11:39:38 +0200

    feat : add portfolio title

--- src/App.js
+++ src/App.js
@@ -13,6 +13,9 @@ import react from './img/langages/react.png';
 import spring from './img/langages/spring.png';
 import ts from './img/langages/ts.png';
 
+import fusee from './img/gif/fusee.gif';
+import pc from './img/gif/pc.gif';
+
 import BandImg from './components/BandImg';
 import Timeline from './components/Timeline';
 
@@ -21,16 +24,29 @@ function App() {
     <div className="App">
       <NavBar />
 
-      <div className='Moi'>
-        <InfoBlock btn_title='Tu souhaite me connaître ? Clique ici !' img_src={panda} texte={<Presentation />} />
-        <InfoBlock btn_title='Tu souhaite me contacter ? clique ici ! ' texte={<Contact />} />
+      <div className="titre-section">
+        <img src={fusee}></img>
+        <div className='titre-desc'>
+          <h1 className="titre">Benjamin ALEXANDRE</h1>
+          <p>Bienvenue sur le portfolio d'un développeur Full Stack</p>
+        </div>
+        <img src={pc}></img>
       </div>
 
-      <div className='langages'>
+      <div className='div'>
+        <h1 className='parte-titre'>Présentation</h1>
+        <div className='Moi'>
+          <InfoBlock btn_title='Tu souhaite me connaître ? Clique ici !' img_src={panda} texte={<Presentation />} />
+          <InfoBlock btn_title='Tu souhaite me contacter ? clique ici ! ' texte={<Contact />} />
+        </div>
+      </div>
+
+      <div className='div'>
+        <h1 className='parte-titre'>Mes compétences technique</h1>
         <BandImg title='Mes langages de programmation préféré' liste_img={[py, django, java, spring, js, ts, react]} />
       </div>
 
-      <div>
+      <div className='div'>
         <Timeline />
       </div>
     </div>
